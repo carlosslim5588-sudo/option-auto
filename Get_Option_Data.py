54894a689858bb4445c55032b9e43fe24abed2d8
carlosslim5588-sudo <carlosslim5588@gmail.com>
2026-08-30 13:44:33 +0900

    Update Get_Option_Data.py

--- Get_Option_Data.py
+++ Get_Option_Data.py
@@ -1,4 +1,5 @@
 # --- 2026/8/13 ---　　PC版（CSV複数_DB前処理Tool.py）と完全一致のDATAが得られること確認済み。https://github.com/　版は取得できない場合の待機時間を設けていることのみ違う。　　
+# ここを変更して　期限を延ばす！！　2026/8/30
 
 import time
 from io import StringIO
